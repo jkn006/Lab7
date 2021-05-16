015e581d4478c10b2828ad0e834c2090d8ad93a0
jkn006 <63220684+jkn006@users.noreply.github.com>
2021-05-16 13:03:40 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -12,36 +12,36 @@
 
 // You will need 3 event listeners:
 //   - One for installation
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  'https://cse110lab6.herokuapp.com/entries'
-];
+// var CACHE_NAME = 'my-site-cache-v1';
+// var urlsToCache = [
+//   'https://cse110lab6.herokuapp.com/entries'
+// ];
 
-self.addEventListener('install', function(event) {
-  // Perform install steps
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
+// self.addEventListener('install', function(event) {
+//   // Perform install steps
+//   event.waitUntil(
+//     caches.open(CACHE_NAME)
+//       .then(function(cache) {
+//         console.log('Opened cache');
+//         return cache.addAll(urlsToCache);
+//       })
+//   );
+// });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(clients.claim());
-});
+// self.addEventListener('activate', event => {
+//   event.waitUntil(clients.claim());
+// });
 
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+// self.addEventListener('fetch', function(event) {
+//   event.respondWith(
+//     caches.match(event.request)
+//       .then(function(response) {
+//         // Cache hit - return response
+//         if (response) {
+//           return response;
+//         }
+//         return fetch(event.request);
+//       }
+//     )
+//   );
+// });
