c80febdb1fc21c9a6fafeb718741c050048e4203
James Nguyen <jkn006@ucsd.edu>
2021-05-16 13:28:58 -0700

    updated sw and script

--- sw.js
+++ sw.js
@@ -2,46 +2,58 @@
 
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
+      navigator.serviceWorker.register('./sw.js').then(function(registration) {
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
       }, function(err) {
         console.log('ServiceWorker registration failed: ', err);
       });
     });
-  }
+}
 
 // You will need 3 event listeners:
 //   - One for installation
-var CACHE_NAME = 'my-site-cache-v1';
-var urlsToCache = [
-  'https://cse110lab6.herokuapp.com/entries'
-];
+//   - One for activation ( check out MDN's clients.claim() for this step )
+//   - One for fetch requests
 
-self.addEventListener('install', function(event) {
+var CACHE_NAME = "journal-site-cache-v1";
+var urlsToCache = ["https://cse110lab6.herokuapp.com/entries"];
+
+self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
           return response;
         }
-        return fetch(event.request);
-      }
-    )
+
+        var responseToCache = response.clone();
+
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
+
+        return response;
+      });
+    })
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(clients.claim());
 });
