6d9105ebb5bd65b7590beae7619d0a22701a9f34
jkn006 <63220684+jkn006@users.noreply.github.com>
2021-05-16 12:42:14 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,67 +1,67 @@
-// // sw.js - Service Worker
+// sw.js - Service Worker
 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//       navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
-//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, function(err) {
-//         console.log('ServiceWorker registration failed: ', err);
-//       });
-//     });
-//   }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 
-// // You will need 3 event listeners:
-// //   - One for installation
-// var CACHE_NAME = 'my-site-cache-v1';
-// var urlsToCache = [
-//   'https://cse110lab6.herokuapp.com/entries'
-// ];
+// You will need 3 event listeners:
+//   - One for installation
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  'https://cse110lab6.herokuapp.com/entries'
+];
 
-// self.addEventListener('install', function(event) {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         console.log('Opened cache');
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-// self.addEventListener('activate', event => {
-//   event.waitUntil(clients.claim());
-// });
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
 
-//         return fetch(event.request).then(
-//           function(response) {
-//             // Check if we received a valid response
-//             if(!response || response.status !== 200 || response.type !== 'basic') {
-//               return response;
-//             }
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response
+            if(!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
 
-//             // IMPORTANT: Clone the response. A response is a stream
-//             // and because we want the browser to consume the response
-//             // as well as the cache consuming the response, we need
-//             // to clone it so we have two streams.
-//             var responseToCache = response.clone();
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
 
-//             caches.open(CACHE_NAME)
-//               .then(function(cache) {
-//                 cache.put(event.request, responseToCache);
-//               });
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
 
-//             return response;
-//           }
-//         );
-//       })
-//     );
-// });
+            return response;
+          }
+        );
+      })
+    );
+});
