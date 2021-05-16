801ee1f9830c4b998270980067cc5f04a9894bb9
jkn006 <63220684+jkn006@users.noreply.github.com>
2021-05-16 14:09:24 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -28,31 +28,31 @@ self.addEventListener("install", function (event) {
   );
 });
 
-// self.addEventListener("fetch", function (event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       // Cache hit - return response
-//       if (response) {
-//         return response;
-//       }
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
 
-//       return fetch(event.request).then(function (response) {
-//         // Check if we received a valid response
-//         if (!response || response.status !== 200 || response.type !== "basic") {
-//           return response;
-//         }
+      return fetch(event.request).then(function (response) {
+        // Check if we received a valid response
+        if (!response || response.status !== 200 || response.type !== "basic") {
+          return response;
+        }
 
-//         var responseToCache = response.clone();
+        var responseToCache = response.clone();
 
-//         caches.open(CACHE_NAME).then(function (cache) {
-//           cache.put(event.request, responseToCache);
-//         });
+        caches.open(CACHE_NAME).then(function (cache) {
+          cache.put(event.request, responseToCache);
+        });
 
-//         return response;
-//       });
-//     })
-//   );
-// });
+        return response;
+      });
+    })
+  );
+});
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(clients.claim());
