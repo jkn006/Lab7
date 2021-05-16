8a1fee85077bac52b0b45d601c3e94bc8b537988
jkn006 <63220684+jkn006@users.noreply.github.com>
2021-05-16 13:05:23 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,47 +1,47 @@
 // sw.js - Service Worker
 
-// if ('serviceWorker' in navigator) {
-//     window.addEventListener('load', function() {
-//       navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
-//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
-//       }, function(err) {
-//         console.log('ServiceWorker registration failed: ', err);
-//       });
-//     });
-//   }
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/Lab7/sw.js').then(function(registration) {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+  }
 
-// You will need 3 event listeners:
-//   - One for installation
-// var CACHE_NAME = 'my-site-cache-v1';
-// var urlsToCache = [
-//   'https://cse110lab6.herokuapp.com/entries'
-// ];
+You will need 3 event listeners:
+  - One for installation
+var CACHE_NAME = 'my-site-cache-v1';
+var urlsToCache = [
+  'https://cse110lab6.herokuapp.com/entries'
+];
 
-// self.addEventListener('install', function(event) {
-//   // Perform install steps
-//   event.waitUntil(
-//     caches.open(CACHE_NAME)
-//       .then(function(cache) {
-//         console.log('Opened cache');
-//         return cache.addAll(urlsToCache);
-//       })
-//   );
-// });
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
 
-// self.addEventListener('activate', event => {
-//   event.waitUntil(clients.claim());
-// });
+self.addEventListener('activate', event => {
+  event.waitUntil(clients.claim());
+});
 
-// self.addEventListener('fetch', function(event) {
-//   event.respondWith(
-//     caches.match(event.request)
-//       .then(function(response) {
-//         // Cache hit - return response
-//         if (response) {
-//           return response;
-//         }
-//         return fetch(event.request);
-//       }
-//     )
-//   );
-// });
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
